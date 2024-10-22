627a954e96f739506eb0d9ab26ec221bca3a474a
Dhruv Patel <dhruvptel2@gmail.com>
2024-10-22 10:47:14 +0000

    Datatypes

---
+++ 01_basic/02_Datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3) // we are using nodejs, not a browser
+
+console.log(3 + 3) // code readability should br high (this is a basic think)
+
+console.log("Dhruv")
+
+// now staring dicussing about Datatypes
+
+let name = "Dhruv" // Datatype : String
+let age = 18    // Datatype : number
+let isloggingin = false // Datatype : boolean
+
+// number => range 2 to poer 52/53
+// bigint (use for bigger number) , smallint (use for smaller number)
+// string => "" (i write in this double quotes)
+// boolean => True/false
+// null => O/p is Object. ( it was empty (blank) )
+// undefined => O/p is undefined
+// symbols => unique
+
+// object
+
+
+console.log(typeof null); // i want to find which type of Datatype is this then i write (typeof "Dhruv")(Output: string)
+console.log(typeof undefined); 
